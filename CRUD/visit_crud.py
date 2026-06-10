72a13150d0a373c884105c163924882d6d6c06cc
rishurajgit <rajrishu.cs@gmail.com>
2026-06-10 19:56:00 +0530

    visit end point

---
+++ CRUD/visit_crud.py
@@ -0,0 +1,36 @@
+from fastapi import HTTPException
+from sqlalchemy.orm import Session
+
+from Models.Visit import Visit
+from Schemas.visit import VisitCreate
+
+
+def create_visit(
+    db: Session,
+    pet_id: int,
+    visit_data: VisitCreate
+):
+
+    try:
+
+        # Create visit object
+        new_visit = Visit(
+            Pet_id=pet_id,
+            Visit_date=visit_data.Visit_date,
+            Reason=visit_data.Reason,
+            Notes=visit_data.Notes
+        )
+
+        # Save to database
+        db.add(new_visit)
+        db.commit()
+        db.refresh(new_visit)
+
+        return new_visit
+
+    except Exception as e:
+
+        raise HTTPException(
+            status_code=500,
+            detail=str(e)
+        )
