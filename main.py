c9600a19860b00262e317b68ea76b3a4e91b4d3b
rishurajgit <rajrishu.cs@gmail.com>
2026-06-10 18:06:24 +0530

    get all pets working fine

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from fastapi import FastAPI
-from database import engine, Base
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import engine, Base, get_db
 from Models.Pets import pet
 from Models.Visit import Visit
+from Schemas.pet import PetCreate, PetResponse
+from CRUD.pet_crud import create_pet as create_pet_db
+from CRUD.pet_crud import  get_all_pets
 
 app = FastAPI()
 
 Base.metadata.create_all(bind = engine)
 
-@app.post("/")
-def post_data():
-    return{
-        "Message":"Database Created"
+@app.post("/pets")
+def create_pet(
+    pet: PetCreate,
+    db: Session = Depends(get_db)
+):
+    return {
+        create_pet_db(db, pet)
     }
+    
+@app.get("/pets")
+def get_pet(
+    db: Session = Depends(get_db)
+):
+    return get_all_pets(db)
