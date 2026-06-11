a05e6d25f34ff4c193c6dc7c38a38ced778d8842
rishurajgit <rajrishu.cs@gmail.com>
2026-06-11 19:59:44 +0530

    Add owner model and schema

--- Schemas/owner.py
+++ Schemas/owner.py
@@ -1,17 +1,17 @@
 from sqlalchemy import UUID, DateTime
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from datetime import datetime
 
 class OwnerCreate(BaseModel):
-    Owner_name : str = Field(min_length=2, max_length=20)
-    Phone : str = Field(min_length=10, max_length=10)
+    Owner_name : str
+    Phone : str 
     Email : str 
     Created_at : DateTime
     
 class OwnerResponse(OwnerCreate):
     ID: UUID
-    Owner_name: str = Field(min_length=2, max_length=20)
-    Phone : str = Field(min_length=10, max_length=10)
+    Owner_name: str 
+    Phone : str 
     Created_at : DateTime
     
 class Config:
