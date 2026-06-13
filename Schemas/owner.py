0f71e5a81041dab3beaf421761288069ffa21f16
rishurajgit <rajrishu.cs@gmail.com>
2026-06-14 05:03:53 +0530

    Fix feature for owner schema

--- Schemas/owner.py
+++ Schemas/owner.py
@@ -1,18 +1,18 @@
-from sqlalchemy import  DateTime
+from sqlalchemy import DateTime
 from pydantic import BaseModel
 from datetime import datetime
 
 class OwnerCreate(BaseModel):
-    owner_name : str
+    name : str
     phone : str 
     email : str 
-    created_at : DateTime
+    # created_at : datetime
     
 class OwnerResponse(OwnerCreate):
     id: int
-    owner_name: str 
+    name: str 
     phone : str 
-    created_at : DateTime
+    created_at : datetime
     
 class Config:
     from_attributes = True
