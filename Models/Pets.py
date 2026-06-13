91e00b100431687fffdbb3a0a568ef24a76736f4
rishurajgit <rajrishu.cs@gmail.com>
2026-06-14 05:02:52 +0530

    Added relationship between pets and owners

--- Models/Pets.py
+++ Models/Pets.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, Column, DateTime
+from sqlalchemy import Integer, String, Column, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
 from datetime import datetime
@@ -11,8 +11,15 @@ class Pet(Base):
     species = Column(String)
     breed = Column(String)
     age = Column(String)
-    owner_name = Column(String)
-    owner_phone = Column(String)
+    # owner_name = Column(String)
+    # owner_phone = Column(String)
     created_at = Column(DateTime, default = datetime.utcnow)
     
+    #one pet can have many visits
     visits = relationship("Visit", back_populates="Pet")
+    
+    #many pets can have same owner
+    
+    owner_id = Column(Integer, ForeignKey("Owner Details.id"))
+    
+    owner = relationship("Owner", back_populates="pets")
